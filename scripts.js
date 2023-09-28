3976ba3203650fc49d1250576b27aab41833990d
dknavar1 <112114119+dknavar1@users.noreply.github.com>
2023-09-28 14:06:28 -0700

    Final js

--- scripts.js
+++ scripts.js
@@ -29,3 +29,29 @@ for (let span of mediumBlueSpans){
     span.style.color = "#3C5E73";
 }
 
+// Selecting a group with a CSS selector
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+
+let svgs = document.querySelectorAll("#svgs svg");
+
+console.log(svgs);
+
+//iterate through the SVG collection and change the color of each
+
+for(let i = 0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+// individual elements with CSS selector
+
+document.querySelector(".bold").style.color = "#7C9EA6";
+
+// changing dom node content, targets first li
+document.querySelector(".content_list li:first-of-type").textContent = "this is new list item text";
+
+// changing dom mode content, targets third li with index number, append +=
+
+document.querySelectorAll(".content_list li")[2].innerHTML += " <strong>I love corgis</strong>";
+
+// removing attributes, challenge activity hint
+document.querySelector("#remove a").removeAttribute("hidden");
